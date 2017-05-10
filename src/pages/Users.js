937d785d72846c0ea8dd3561d095988e21325000
Mrmaxmeier <Mrmaxmeier@gmail.com>
2017-05-10 20:58:01 +0200

    add link to user

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import Relay from 'react-relay'
 import { Table } from 'semantic-ui-react'
+import { NavigationLink } from 'navigation-react'
 
 import { App } from '../App.js'
 import { relayContainer } from '../decorators.js'
@@ -9,6 +10,7 @@ import { relayContainer } from '../decorators.js'
   fragments: {
     user: () => Relay.QL`
       fragment on User {
+        id,
         username,
         email,
         admin
@@ -18,13 +20,21 @@ import { relayContainer } from '../decorators.js'
 })
 export class ListEntry extends PureComponent {
   static propTypes = {
-    user: React.PropTypes.object
+    user: React.PropTypes.object,
+    stateNavigator: React.PropTypes.object
   }
   render () {
-    let { username, email, admin } = this.props.user
+    let { id, username, email, admin } = this.props.user
     return (
       <Table.Row key={username}>
-        <Table.Cell>{username}</Table.Cell>
+        <Table.Cell>
+          <NavigationLink
+            stateKey="user"
+            navigationData={{userID: id}}
+            stateNavigator={this.props.stateNavigator}>
+            {username}
+          </NavigationLink>
+        </Table.Cell>
         <Table.Cell>{email}</Table.Cell>
         <Table.Cell>{admin ? 'Yes' : 'No'}</Table.Cell>
       </Table.Row>
@@ -61,7 +71,9 @@ export class UsersPage extends PureComponent {
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {this.props.userStore.users.map((data, i) => <ListEntry key={i} user={data} />)}
+            {this.props.userStore.users.map((data, i) =>
+              <ListEntry key={i} user={data} stateNavigator={this.props.stateNavigator} />
+            )}
           </Table.Body>
         </Table>
       </App>
